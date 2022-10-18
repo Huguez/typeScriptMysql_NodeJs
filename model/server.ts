cf407c71b28c54dc32b39a96701e8fc65fa90e02
Huguez <carlos.huguez@gmail.com>
2022-10-17 20:10:15 -0700

    primeros pasos del app

---
+++ model/server.ts
@@ -0,0 +1,25 @@
+import express,{ Application } from "express";
+
+class Server {
+   
+   private app: Application ;
+   private port: string;
+
+   constructor(){
+      this.app = express();
+      this.port = process.env.PORT || "8000";
+
+   }
+
+
+
+   start(){
+      this.app.listen( this.port,  () =>{
+         console.log(`Server stared!!!`);
+         console.log(`Go to http://localhost:${ this.port }/`);
+      } )
+   }
+   
+}
+
+export default Server;
